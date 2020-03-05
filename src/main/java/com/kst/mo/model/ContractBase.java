6514e18db1f2d3db5ab7fdd4b1f2386b3e23fda8
박종석 <plannist19@gmail.com>
2020-03-05 19:06:04 +0900

    로그 / VO 수정 커밋

--- src/main/java/com/kst/mo/model/ContractBase.java
+++ src/main/java/com/kst/mo/model/ContractBase.java
@@ -1,5 +1,7 @@
 package com.kst.mo.model;
 
+import java.sql.Timestamp;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -8,4 +10,18 @@ import lombok.EqualsAndHashCode;
 public class ContractBase {
 	private String name;
 	
+	private Long ctNo;
+	private String ctNm;
+	private String mdlNm;
+	private String mdlColor;
+	private String cellFstNo;
+	private String cellMdlNo;
+	private String cellLstNo;
+	private String zipCd;
+	private String baseAddr;
+	private String dtlAddr;
+	private String argYn;
+	private String payYn;
+	private Timestamp sysRegDt;
+	private Timestamp sysModDt;
 }
